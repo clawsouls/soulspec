fb1a5c2ed643c91f69e167c22da6faeee6d52ea7
Tom Jaejoon Lee <tom.jaejoon@gmail.com>
2026-02-27 09:22:00 +0900

    fix: baseUrl / for custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://clawsouls.github.io',
-  baseUrl: '/soulspec/',
+  baseUrl: '/',
 
   organizationName: 'clawsouls',
   projectName: 'soulspec',
